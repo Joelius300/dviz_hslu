30915dd0c94fce2209e63b26305b16ed2dd0ee63
Joel L <joelius300@protonmail.com>
2022-12-05 18:37:54 +0100

    Paper prototyp and feedback

--- main.py
+++ main.py
@@ -5,3 +5,41 @@ import plotly.express as px
 
 if __name__ == "__main__":
     pass
+
+
+# TODO
+# Line chart with average stored energy ((puffer oben + puffer unten) / 2) fan'd out to 50% on each site.
+# Line color green or yellow (just try). fan color just a lighter, more translucent color.
+# By default hidden but optionally displayed: The actual puffer oben and puffer unten values with red (or orange) and blue lines.
+# Legend maybe above?
+# Additionally, display a lower and upper threshold with a dashed or dotted line (in black?) but make sure it's not the same
+# as the cursor indicator line.
+# If you have enough time add a water buffer tank to the right of it with red at the top phasing out downwards
+# and blue at the bottom phasing out upwards with either the respective low/high values or the average value in the center.
+# The whole thing should be zoomable and panable
+# Data is selected with a time selector
+# Last week or 5 days by default
+# Then another chart with the same lower and upper threshold and the same time selection, which just shows a line for the
+# boiler temp. These thresholds should be configurable just like the time selection.
+
+# Notes:
+# - In summer the boiler is important, in winter the buffer
+# - The dashboard is meant for current data and supposed to assist in the analytical task of determining the next best
+#   point in time for firing up the furnace. Old data may be interesting to look at but not the main purpose of it.
+# - the drinking water temperature doesn't make sense, it of course can never be heated above the maximum stored energy
+#   (it can stay there longer than the buffer sure but it can't rise above the buffer max!)
+
+# Feedback:
+# - Danger zone -> alert
+# - Very clear indication to get an instruction without having to analyze the chart -> light signal (and maybe a time estimate)
+# - Shift data maybe by 1 year back so you have access to "future" values
+# - Compare summer and winter and try to analyze whether or not the the average is even meaningful (in winter)
+# - Maybe show the real data by default and have a simplified checkbox instead of a detailed to show the average
+#   -> she's concerned that the average of the two points are misleading or not helpful when the real
+#      distribution is unknown
+# - Maybe figure out which is more important for the decision, buffer max, buffer min or average.
+#   -> Dad says the max is (much) more important currently but the average might give interesting insight
+
+# Questions I didnt ask:
+# - Visual buffer drawn next to it (probably not helpful or impressive, think we can ignore that)
+# - Conversion into kwh or smth like that but what out she's a physicist so it would have to be right and that's hard
